3b6f2a494c8179416b69c1048fa1e57d9c744113
Shrijana Maharjan <shrezana2060@gmail.com>
2024-05-09 04:51:42 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Routes, Route} from "react-router-dom";
+import Register from "./component/Register";
+import Login from "./component/Login";
+import Contact from "./component/Contact";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import Services from "./component/Services";
+import Team from "./component/Team";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+
+    <Routes >
+      <Route path="/" element={ <Home /> } />
+      <Route path="/Login" element={ <Login /> } />
+      <Route path="/team" element={ <Team /> } />
+      <Route path="/Register" element={ <Register /> } />
+      <Route path="/services" element={ <Services /> } />
+      <Route path="/contact" element={ <Contact /> } />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Contact() {
+  return (
+    <div>
+      <h1 className='bg-success text-white p-5'>Welcome to learnign app</h1>
+    </div>
+  )
+}
+
+export default Contact
