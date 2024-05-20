1439584fb1c6b677e727b26b65e9e79466acbb6e
Shrijana Maharjan <shrezana2060@gmail.com>
2024-05-20 09:15:22 -0700

    feat:Register form added

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,32 +1,52 @@
-import React from 'react'
+import React from 'react';
+import "./../component/Register.css";
 
-function Register() {
+const Register = () => {
   return (
     <div class="flex flex-col justify-between max-w-xl px-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen-xl">
-  <div class="pt-16 mb-16 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
+      <div class="pt-16 mb-16 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
     <div class="max-w-xl mb-6">
       
-      <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl sm:leading-none">
+      {/* <h2 class="max-w-lg mb-2 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl sm:leading-none">
         Register US
-      </h2>
+      </h2> */}
       <p class="text-base text-gray-700 md:text-lg">
-        <h1>Simple & Fast</h1>
-        <h2>Payment</h2>
+        <h1>Simple & Fast Payment</h1>
+        
       </p>
     </div>
     <div class="flex items-center">
       <div class="btn-group">
-      <a href="/" class="btn btn-dark active" aria-current="page">Learn More--</a>
+      <a href="/Register" class="btn btn-dark active" aria-current="page">Register Now--></a>
  
       </div>
      
     </div>
   </div>
-  <div>
-    <img src="https://kitwind.io/assets/kometa/two-thirds-phone.png" class="object-cover object-top w-full h-64 mx-auto lg:h-auto xl:mr-24 md:max-w-sm" alt="" />
-  </div>
-</div>
-  )
-}
+    <div className="register-container">
+      <div className="register-form">
+        <h2>CREATE ACCOUNT</h2>
+        <form>
+          <input type="text" placeholder="Enter your Name" required />
+          <input type="email" placeholder="Enter your Email" required />
+          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm password" required />
+          <div className="terms">
+            <input type="checkbox" id="terms" required />
+            <label htmlFor="terms">
+              I agree to all statements in <a href="#">Terms of Service</a>
+            </label>
+          </div>
+          
+          <button type="submit">SIGN UP</button>
+        </form>
+        <p>
+          Have already an account? <a href="/Login">Login here</a>
+        </p>
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
